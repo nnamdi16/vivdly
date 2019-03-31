197900c7c04c1fafa21524e5f6573d30580511be
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-03-31 03:17:43 +0100

    Connect vivdly to mongodb database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const mongoose = require('mongoose');
 const movies = require('./routes/genres');
 const express = require('express');
 const app = express();
@@ -6,6 +6,9 @@ const app = express();
 app.use(express.json());
 app.use('/api/movies',movies);
 
+mongoose.connect('mongodb://localhost/vivdly')
+  .then(() => console.log('Connected to MongoDb...'))
+  .catch(err => console.error('Could not connect to Mongodb....',err));
 
 //validate course function is used to validate the schema using Joi
 
