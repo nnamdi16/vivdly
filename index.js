35f02cd83e4843dce6bf060a61a6a398da6890bf
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-02 13:18:17 +0100

    Imported the movies function in index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use('/api/genres',genres);
 app.use('/api/customers',customers);
+app.use('/api/movies',movies);
 
 mongoose.connect('mongodb://localhost/vivdly')
   .then(() => console.log('Connected to MongoDb...'))
